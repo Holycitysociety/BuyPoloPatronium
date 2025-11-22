39809d63b4c13b7a11a27854a038311509dd89f5
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2025-11-21 19:25:14 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThirdwebProvider } from "thirdweb/react";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThirdwebProvider>
+      <App />
+    </ThirdwebProvider>
   </React.StrictMode>
 );
